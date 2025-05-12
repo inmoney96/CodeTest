1421fcdbbc1f6ec8c6bb6e22bf0281b3dec85782
inmoney96 <60872614+inmoney96@users.noreply.github.com>
2025-05-12 14:53:18 +0900

    [Silver IV] Title: 상근이의 여행, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	for (int i = 0;i < t;++i)
+	{
+		int n, m; cin>>n>>m;
+		for (int j = 0;j < m;++j)
+		{
+			int a, b;
+			cin >> a >> b;
+		}
+		cout << n-1 << endl;
+	}
+	return 0;
+}
