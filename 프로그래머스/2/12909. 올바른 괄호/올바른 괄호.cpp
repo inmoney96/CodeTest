b614655bac50c274e4d996f43ee203182881f63a
inmoney96 <60872614+inmoney96@users.noreply.github.com>
2025-06-23 15:20:36 +0900

    [level 2] Title: 올바른 괄호, Time: 0.14 ms, Memory: 3.78 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -5,13 +5,7 @@ using namespace std;
 
 bool solution(string s)
 {
-    bool answer = true;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    cout << "Hello Cpp" << endl;
-
     int n = 0;
-
     for (int i = 0; i < s.size(); ++i)
     {
         if (s[i] == '(') {
@@ -29,5 +23,5 @@ bool solution(string s)
         return false;
     }
 
-    return answer;
+    return true;
 }
