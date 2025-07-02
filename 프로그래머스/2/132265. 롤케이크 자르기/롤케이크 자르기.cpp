a5fbfe6b341c4631ec42978445fc8978eee1d30b
inmoney96 <60872614+inmoney96@users.noreply.github.com>
2025-07-02 17:04:13 +0900

    [level 2] Title: 롤케이크 자르기, Time: 34.91 ms, Memory: 49.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<int> topping) {
+    int answer = 0;
+	int l = 0, r = 0;
+    vector<stack<int>> LTopping(10001);
+    vector<stack<int>> RTopping(10001);
+
+    for (int i = 0; i < topping.size(); i++) {
+		if (RTopping[topping[i]].empty()) {
+			r++;
+		}
+        RTopping[topping[i]].push(topping[i]);
+    }
+
+	for (int i = 0; i < topping.size(); i++) {
+		RTopping[topping[i]].pop();
+		if (RTopping[topping[i]].empty()) {
+			r--;
+		}
+		if (LTopping[topping[i]].empty()) {
+			l++;
+		}
+		LTopping[topping[i]].push(topping[i]);
+		if (l == r) {
+			answer++;
+		}
+	}
+
+    return answer;
+}
